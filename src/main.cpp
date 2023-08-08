1cbf5b941c5814f8b283fdeb11406d00618ebdb3
DavidJedeikin <david.jedeikin@gmail.com>
2023-08-08 19:41:19 +0100

    Playing with full robot

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,36 @@
 Servo rightArm;
 Servo leftArm;
 Servo base;
-#define DELAY_MS 5
+
+// #define START 80
+// #define END 175
+
+#define START 0
+#define END 180
+
+#define DELAY_MS 200
 
 void setup()
 {
   hugo::SerialLogger::getInstance().init();
   rightArm.attach(A0);
   leftArm.attach(A1);
-  base.attach(A2);
+  // base.attach(A2);
 }
 
 void loop()
 {
   int pos;
-  LOG_RAW("%s", "Driving from: 0:180");
-  for (pos = 0; pos <= 180; pos += 1)
+  LOG_RAW("Driving from: %u:%u, Delay: %ums", START, END, DELAY_MS);
+  for (pos = START; pos <= END; pos += 1)
   {
     rightArm.write(pos);
     leftArm.write(pos);
     base.write(pos);
     delay(DELAY_MS);
   }
-  LOG_RAW("%s", "Driving from: 180:0");
-  for (pos = 180; pos >= 0; pos -= 1)
+  LOG_RAW("Driving from: %u:%u, Delay: %ums", END, START, DELAY_MS);
+  for (pos = END; pos >= START; pos -= 1)
   {
     rightArm.write(pos);
     leftArm.write(pos);
