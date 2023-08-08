bc6fb6f7283216f69d135408f1cc501abddc05df
DavidJedeikin <david.jedeikin@gmail.com>
2023-08-08 18:54:30 +0100

    Basic robot working

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,40 @@
 #include "serialLogger.hpp"
 #include <Arduino.h>
 
-// #include <Servo.h>
+#include <Servo.h>
 #include <Wire.h>
 #include <string>
+
+Servo rightArm;
+Servo leftArm;
+Servo base;
+#define DELAY_MS 5
 
 void setup()
 {
   hugo::SerialLogger::getInstance().init();
+  rightArm.attach(A0);
+  leftArm.attach(A1);
+  base.attach(A2);
 }
 
 void loop()
 {
-  int i = 0;
-  float height = 5.7F;
-  std::string name = "David";
-  while (true)
+  int pos;
+  LOG_RAW("%s", "Driving from: 0:180");
+  for (pos = 0; pos <= 180; pos += 1)
   {
-    LOG_RAW("A: %d, Height: %.2f, Name: %s \n\r", i++, height, name.c_str());
-    delay(100);
+    rightArm.write(pos);
+    leftArm.write(pos);
+    base.write(pos);
+    delay(DELAY_MS);
+  }
+  LOG_RAW("%s", "Driving from: 180:0");
+  for (pos = 180; pos >= 0; pos -= 1)
+  {
+    rightArm.write(pos);
+    leftArm.write(pos);
+    base.write(pos);
+    delay(DELAY_MS);
   }
 }
