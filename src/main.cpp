403d085faa80d04a3990a0d8f0bbfe02124689fa
DavidJedeikin <david.jedeikin@gmail.com>
2023-08-12 18:09:34 +0100

    Sonar sensor array working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "log.hpp"
-// #include <Adafruit_TinyUSB.h> // for Serial
+#include <Adafruit_TinyUSB.h> // for Serial
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -14,12 +14,14 @@ void loop()
   //////////////////////////////////////////////////////////////////////
   hugo::SerialLogger::getInstance().init();
 
-  int triggerPin{30};
-
   int rightEchoPin{11};
-  int middleEchoPin{31};
+  int rightTriggerPin{7};
 
-  pinMode(triggerPin, OUTPUT);
+  int middleEchoPin{31};
+  int middleTriggerPin{30};
+
+  pinMode(rightTriggerPin, OUTPUT);
+  pinMode(middleTriggerPin, OUTPUT);
 
   pinMode(rightEchoPin, INPUT);
   pinMode(middleEchoPin, INPUT);
@@ -36,20 +38,29 @@ void loop()
   static constexpr float MICROSECONDS_TO_SECONDS{1 / 1000000.0F};
   while (true)
   {
-    // Trigger All
-    digitalWrite(triggerPin, LOW);
+    // Right
+    digitalWrite(rightTriggerPin, LOW);
     delayMicroseconds(2); // Just to make sure you start high
-    digitalWrite(triggerPin, HIGH);
+    digitalWrite(rightTriggerPin, HIGH);
     delayMicroseconds(10);
-    digitalWrite(triggerPin, LOW);
-
-    // Receive Echo
-
-    // distanceCmMiddle = (pulseIn(middleEchoPin, HIGH) / 2.0F) *
-    //                    MICROSECONDS_TO_SECONDS * SPEED_OF_SOUND * METERS_TO_CM;
+    digitalWrite(rightTriggerPin, LOW);
 
     distanceCmRight = (pulseIn(rightEchoPin, HIGH) / 2.0F) *
                       MICROSECONDS_TO_SECONDS * SPEED_OF_SOUND * METERS_TO_CM;
+
+    delay(100);
+
+    // Middle
+    digitalWrite(middleTriggerPin, LOW);
+    delayMicroseconds(2); // Just to make sure you start high
+    digitalWrite(middleTriggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(middleTriggerPin, LOW);
+
+    distanceCmMiddle = (pulseIn(middleEchoPin, HIGH) / 2.0F) *
+                       MICROSECONDS_TO_SECONDS * SPEED_OF_SOUND * METERS_TO_CM;
+
+    delay(100);
 
     LOG_RAW("Right: %.0f, Middle: %.0f", distanceCmRight, distanceCmMiddle);
   }
